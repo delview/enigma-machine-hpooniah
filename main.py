0e6858f0a8584e09d8acf05cfa1a87a70217887c
hpooniah <harmanpreetp457@deltalearns.ca>
2025-03-06 21:30:44 +0000

    finished the introduction

--- main.py
+++ main.py
@@ -5,9 +5,14 @@
 # Encrypt function
 
 # Greet the user
+print("Hello, This is a phone chraracter cipher program")
+name = input("Before we begin, what is your name? ")
+print(f"Hello {name} lets get started")
+
 
 # Ask if they want to decrypt or encrypt a message.
-
+choice = input("Would you like to [d]ecrypt or [e]ncrypt a message? ")
+if choice == 
 # Call the function based on the users choice
 
 # Decrypt the message
