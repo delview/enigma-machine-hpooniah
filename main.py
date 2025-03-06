ed010fc814cf19385cf37903bbd7cf14eeab678d
hpooniah <harmanpreetp457@deltalearns.ca>
2025-03-06 21:10:19 +0000

    Done the pseudocode

---
+++ main.py
@@ -0,0 +1,23 @@
+# Start Enigma Machine
+
+# decrypt function 
+
+# Encrypt function
+
+# Greet the user
+
+# Ask if they want to decrypt or encrypt a message.
+
+# Call the function based on the users choice
+
+# Decrypt the message
+
+# Encrypt the message
+
+# Print the decripted message 
+
+# Print the encrypted message
+
+# Ask the user if thye want to decrypt or encrypt another message.
+
+# write a goodbye message
