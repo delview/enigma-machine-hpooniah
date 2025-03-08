7950a88b299332b434289f6dc829de0066f0f89f
hpooniah <harmanpreetp457@deltalearns.ca>
2025-03-08 23:16:24 +0000

    I added the phone characters and the letter they need to match with

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 # Start Enigma Machine
 
+# Phone charcters matching
+
+phone_charcteers_matching = {
+"2": ["A", "B", "C"],
+"3": ["D", "E", "F"],
+"4": ["G", "H", "I"],
+"5": ["J", "K", "L"],
+"6": ["M", "N", "O"],
+"7": ["P", "Q", "R", "S"],
+"8": ["T", "U", "V"],
+"9": ["W", "X", "Y", "Z"],
+
+
+
+}
+
+
+
 # decrypt function 
 
 # Encrypt function
@@ -11,8 +29,10 @@ print(f"Hello {name} lets get started")
 
 
 # Ask if they want to decrypt or encrypt a message.
-choice = input("Would you like to [d]ecrypt or [e]ncrypt a message? ")
-if choice == 
+choice = input("Would you like to [d]ecrypt or [e]ncrypt a message? ").strip().lower()
+if choice == d: 
+
+
 # Call the function based on the users choice
 
 # Decrypt the message
